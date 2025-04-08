ff84d28770c47e1e2b9b7b47a4721e850d6d44d2
Seojin Kang <jin040205@gmail.com>
2025-04-08 13:43:57 +0900

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-# config.py 파일 생성
-WEATHER_API_KEY = "7PjattrxPKa0He1GbJAlAAFpR%2FtxSIyaIRwuCwEgQZCxRyh42Y3LYWWn0vrh7rovkK753c8cOqnqOeGQly%2FSqQ%3D%3D"
-NAVER_CLIENT_ID = ""rXrsRKFlflMjL3Vm1VP4
-NAVER_CLIENT_SECRET = "rdKxzozaql"
